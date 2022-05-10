d19714ef0c16bd2adba8bc4f06e7c5ee6663e0aa
arietwigt <twigt.arie@gmail.com>
2022-05-10 14:27:34 +0200

    added examples for the standard library

---
+++ 3_exp_standard_library_2.py
@@ -0,0 +1,10 @@
+#%%
+from math import pow, pi, sqrt
+import datetime as d
+
+#%%
+pi
+# %%
+sqrt(144)
+# %%
+
---
+++ 3_exp_standard_library_os.py
@@ -0,0 +1,30 @@
+#%%
+import os
+# %%
+os.getcwd()
+# %%
+os.mkdir("my_folder")
+# %%
+with open("my_folder/my_file.txt", "w") as file:
+    file.write("Hello") 
+# %%
+os.listdir()
+# %%
+os.listdir("my_folder")
+# %%
+available_files = os.listdir("my_folder")
+
+#%%
+file_to_remove = "my_file.txt"
+
+
+#%%
+if file_to_remove in available_files:
+    print("Removing file")
+    os.remove(f"my_folder/{file_to_remove}")
+else:
+    print("File not found, proceeding")
+
+# %%
+os.rmdir("my_folder")
+# %%
