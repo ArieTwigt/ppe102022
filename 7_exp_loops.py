9574ae5e1f489cc3583fc7f1c9d7f5ff6b2ee578
arietwigt <twigt.arie@gmail.com>
2022-05-12 15:32:01 +0200

    added example with infinite lop[

---
+++ 7_exp_loops.py
@@ -0,0 +1,22 @@
+#%%
+name_list = ['arie', 'james', 'henk', 'karin']
+
+for name in name_list:
+    print(name)
+
+# %%
+for idx, name in enumerate(name_list):
+    print(f"De naam {name} staat op positie {idx + 1}")
+
+# %%
+import datetime
+
+current_time = datetime.datetime.now()
+current_time_10secs = current_time + datetime.timedelta(seconds=10)
+
+# %%
+while current_time < current_time_10secs:
+    
+    print("We zijn bezig")
+
+# %%
