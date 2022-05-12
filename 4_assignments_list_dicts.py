3a7e965e28fd450c3e052e2c04fd172d296a2694
arietwigt <twigt.arie@gmail.com>
2022-05-12 10:45:41 +0200

    added assignments for dictionaries

---
+++ 4_assignments_list_dicts.py
@@ -0,0 +1,34 @@
+## Assignments
+
+#%% 1a. 
+person = {}
+person['name'] = "Clark"
+person['age'] =  40
+person['hobbies'] = ['football', 'tennis', 'mountainbike']
+
+
+#%% 1b.
+person = {'name': 'Clark', 
+          'age': 40, 
+          'hobbies': ['football', 'tennis', 'mountainbike']}
+
+
+#%% 2. 
+person_2 = {"name": "Mary", "age": 43}
+person_3 = {"name": "Jim", "age": 14, "hobbies": ['gaming', 'football']}
+person_4 = {"name": "Rose", "age": 20}
+
+#%%
+family_dict = {}
+family_dict['name'] = "Kent"
+family_dict['members'] = [person, person_2, person_3, person_4]
+
+
+# %%
+family_dict
+
+#%% BONUS
+# Append the following person to the members of family dict
+person_5 = {'name': 'peter', 'age': 0}
+
+#?
