3e7c30335bc57191f732904419f8743f142f07c6
arietwigt <twigt.arie@gmail.com>
2022-05-13 15:32:57 +0200

    added example text writer

--- 10_filewriter.py
+++ 10_filewriter.py
@@ -1,13 +1,54 @@
 import os
 
-available_files_folders = "\n".join(os.listdir())
+available_folders = []
+
+# check if the file is a folder
+current_files_folders = os.listdir()
+
+# provide the list of folders
+for file_folder in current_files_folders:
+    if os.path.isdir(file_folder):
+        if file_folder[0] == ".": # exclude hidden files like '.git'
+            continue
+        
+        available_folders.append(file_folder)
+    else:
+        continue
 
 
-folder_name = input(f"Name of the folder:\n Available folders\n {available_files_folders}\n")
-file_name = input("Name of the file:\n")
+# format and sort the availale folders
+available_folders = "\t".join(sorted(available_folders))
+
+# select the folder
+folder_name = input(f"\nAvailable folders\n\n {available_folders}\n\nName of the folder:\n")
+
+# format the folder name
+folder_name = folder_name.replace(" ", "_").lower()
+
+if folder_name not in current_files_folders:
+    os.mkdir(folder_name)
+
+# show the available files in the current folder
+files_in_folder = os.listdir(folder_name)
+
+available_files = []
+
+for file in files_in_folder:
+    if os.path.isfile(f"{folder_name}/{file}"):
+        available_files.append(file)
+        
+# format and sort the available files
+available_files = "\t".join(sorted(available_files))
+
+# take the file name from the input
+file_name = input(f"\nAvailable files\n\n {available_files}\n\nName of the file:\n")
+
+# make sure we remove the extension from the file name
+file_name = file_name.split(".")[0]
+
+# decide the write_mode
 write_mode = input("Do you want to overwrite?(Y/n)") or "y"
 
-folder_name = folder_name.replace(" ", "_").lower()
 file_name = file_name.replace(" ", "_").lower()
 
 if write_mode.lower() == "y":
@@ -16,12 +57,6 @@ if write_mode.lower() == "y":
 else:
     print(f"Inserted {write_mode}. We will append to the textfile")
     write_mode = "a"
-
-
-current_files_folders = os.listdir()
-
-if folder_name not in current_files_folders:
-    os.mkdir(folder_name)
 
 
 content = input("Insert your story:\n\n")
