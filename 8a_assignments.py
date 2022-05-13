6ff7d8347f1a61be0a7b2d8c3aaaa8ad4ef7ad96
arietwigt <twigt.arie@gmail.com>
2022-05-13 10:51:20 +0200

    added additional examples for the functions

--- 8a_assignments.py
+++ 8a_assignments.py
@@ -25,20 +25,44 @@ print(result)
 
 # Assignment 2
 ## Define a function that accepts a lists, capitalizes every name in the list, and retuns this capitalized list. You can use: ['Jim', 'John', 'Marc', 'Danny', 'Peter']
-# %% define the names list
-my_names = ['jim', 'john', 'marc', 'danny', 'peter']
 
 
 #%% define the function
-def capitalize_names(names_list):
-    new_list = []
+def capitalize_list(names_list: list, auto_convert=False) -> list:
 
+    # validate the input as a list
+    if type(names_list) != list:
+        if auto_convert:
+            names_list = [names_list]
+        else:
+            raise TypeError("The input should be a list")
+
+
+    new_list = []
+    
     for name in names_list:
-        new_list.append(name.capitalize())
+        name_capitalized = name.capitalize()
+        new_list.append(name_capitalized)
     
     return new_list
 
 
+
+# %% define the names list
+my_names = ['jim', 'john', 'marc', 'danny', 'peter']
+
+#%%
+capitalize_list(my_names)
+
 #%% apply the function
-capitalize_names(my_names)
+capitalize_list("jan", auto_convert=True)
+
+
+# %%
+
+naam = "Arie"
+
+
+for name in naam:
+    print(name)
 # %%
